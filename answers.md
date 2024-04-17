eff17c67aa9f8421a312e78e43d000b3cc18c1ef
dtcicero <130113355+dtcicero@users.noreply.github.com>
2024-04-17 22:33:44 +0000

    sp24-recitation-07-dtcicero

--- answers.md
+++ answers.md
@@ -1,6 +1,6 @@
 # CMPS 2200 Recitation 10## Answers
 
-**Name:**_________________________
+**Name:** Daniel Cicero
 **Name:**_________________________
 
 
@@ -9,9 +9,14 @@ Place all written answers from `recitation-07.md` here for easier grading.
 
 
 - **2)**
+-  Given n nodes and m edges, the work of reachable is O(n + m) The worst case arises in a graph in which all nodes are connected to each other.
+
 
 - **4)**
+- Given n nodes in a graph, the worst case number of times we would have to call reachable is n times.  
 
 - **5)**
+- Assuming n nodes and m edges, the work of connected is O(n+m)
 
 - **7)**
+- Switching from a map of neighbors (adjacency list) to an adjacency matrix would indeed affect the work complexity of the reachable function. This is because in the adjacency list, checking for neighbors can be accomplished by iterating over the neighbors of each node. As each node can have a maximum of n neighbors, the work is O(m) + and additional step of O(n) for initialization for a total of O(n + m) work. By contrast, the adjacency matrix represents neighbors as an n * n matrix, and therefore requires O(n^2) work to initialize. Neighbor exploration in the adjacency matrix can be done in constant time, but iterating over all neighbors of a node still takes O(n) time due to the number of entries in the row corresponding to that node.
--- main.py
+++ main.py
@@ -10,33 +10,48 @@ def make_undirected_graph(edge_list):
 
 
 def reachable(graph, start_node):
-    """
-    Returns:
-      the set of nodes reachable from start_node
-    """
-    result = set([start_node])
-    frontier = set([start_node])
-    while len(frontier) != 0:
-        ###TODO
-        pass
-    return result
+  """
+  Returns:
+  the set of nodes reachable from start_node
+"""
+  result = set([start_node])
+  frontier = set([start_node])
+  while frontier:
+      current_node = frontier.pop()
+      for neighbor in graph[current_node]:
+          if neighbor not in result:
+              result.add(neighbor)
+              frontier.add(neighbor)
+  return result
 
 
 
 
 
 def connected(graph):
-    ### TODO
-    pass
 
+  if not graph:
+      return False  
+  start_node = next(iter(graph))  
+  reachable_nodes = reachable(graph, start_node)
+
+  return len(reachable_nodes) == len(graph)
 
 
 
 def n_components(graph):
-    """
-    Returns:
-      the number of connected components in an undirected graph
-    """
-    ### TODO
-    pass
+
+  if not graph:
+      return 0 
+
+  num_components = 0
+  remaining_nodes = set(graph.keys())
+
+  while remaining_nodes:
+      start_node = remaining_nodes.pop()
+      reachable_nodes = reachable(graph, start_node)
+      num_components += 1
+      remaining_nodes -= reachable_nodes
+
+  return num_components
 
